5dd37931473c668c6c3cec2ccfe270687e806b41
SamraJ37 <sjafarli17864@ada.edu.az>
2026-02-26 00:31:02 +0400

    Add ProductRepositoryImpl implementation

--- src/main/java/az/edu/ada/wm2/lab4/Lab4Application.java
+++ src/main/java/az/edu/ada/wm2/lab4/Lab4Application.java
@@ -1,13 +1,18 @@
 package az.edu.ada.wm2.lab4;
 
+import az.edu.ada.wm2.lab4.repository.ProductRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab4Application {
 
+	@Autowired
+	private ProductRepositoryImpl productRepository;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(Lab4Application.class, args);
 	}
-
 }
