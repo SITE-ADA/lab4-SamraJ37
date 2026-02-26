be27fe1669be6fa9e26314522b9bb87a7ca76566
SamraJ37 <sjafarli17864@ada.edu.az>
2026-02-26 10:11:09 +0400

    Update Product class

--- src/main/java/az/edu/ada/wm2/lab4/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab4/model/Product.java
@@ -4,60 +4,107 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+
+
 public class Product {
 
-    private UUID id;
     private String productName;
     private BigDecimal price;
     private LocalDate expirationDate;
+    private UUID id;
+
+
 
     public Product() {
+
         this.id = UUID.randomUUID();
+
     }
+
+
 
     public Product(String productName, BigDecimal price, LocalDate expirationDate) {
+
         this.id = UUID.randomUUID();
+
         this.productName = productName;
+
         this.price = price;
+
         this.expirationDate = expirationDate;
+
     }
+
+
 
     public Product(UUID id, String productName, BigDecimal price, LocalDate expirationDate) {
+
         this.id = id;
+
         this.productName = productName;
+
         this.price = price;
+
         this.expirationDate = expirationDate;
+
     }
+
+
 
     public UUID getId() {
+
         return id;
+
     }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+
 
     public String getProductName() {
+
         return productName;
+
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
+
 
     public BigDecimal getPrice() {
+
         return price;
+
     }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 
-    public LocalDate getExpirationDate() {
+
+    public LocalDate getExpirationDate(){
+
         return expirationDate;
+
     }
 
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
+
+
+    public void setProductName(String productName){
+
+        this.productName = productName;
+
     }
+
+
+
+    public void setPrice(BigDecimal price){
+
+        this.price = price;
+
+    }
+
+
+
+    public void setExpirationDate(LocalDate expirationDate){
+
+        this.expirationDate = expirationDate;
+
+    }
+
+
+
 }
