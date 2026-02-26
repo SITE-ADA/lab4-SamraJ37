820c965f6ff05e6b0f1e550133840fa596ae2363
SamraJ37 <sjafarli17864@ada.edu.az>
2026-02-26 10:26:12 +0400

    uptade ProductController

--- src/main/java/az/edu/ada/wm2/lab4/controller/ProductController.java
+++ src/main/java/az/edu/ada/wm2/lab4/controller/ProductController.java
@@ -1,8 +1,7 @@
 package az.edu.ada.wm2.lab4.controller;
 
-import az.edu.ada.wm2.lab4.model.Product;
 import az.edu.ada.wm2.lab4.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import az.edu.ada.wm2.lab4.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,39 +17,43 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.createProduct(product);
-        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+        try {
+            Product created = productService.createProduct(product);
+            return new ResponseEntity<>(created, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
-        List<Product> products = productService.getAllProducts();
-        return new ResponseEntity<>(products, HttpStatus.OK);
+        try {
+            return ResponseEntity.ok(productService.getAllProducts());
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable UUID id) {
         try {
-            Product product = productService.getProductById(id);
-            return new ResponseEntity<>(product, HttpStatus.OK);
-        } catch (RuntimeException e) {
+            return ResponseEntity.ok(productService.getProductById(id));
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable UUID id, @RequestBody Product updatedProduct) {
+    public ResponseEntity<Product> updateProduct(@PathVariable UUID id, @RequestBody Product product) {
         try {
-            Product product = productService.updateProduct(id, updatedProduct);
-            return new ResponseEntity<>(product, HttpStatus.OK);
-        } catch (RuntimeException e) {
+            return ResponseEntity.ok(productService.updateProduct(id, product));
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -60,21 +63,29 @@ public class ProductController {
         try {
             productService.deleteProduct(id);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @GetMapping("/filter/expiring")
-    public ResponseEntity<List<Product>> getProductsExpiringBefore(@RequestParam("date") String date) {
-        LocalDate expirationDate = LocalDate.parse(date);
-        List<Product> products = productService.getProductsExpiringBefore(expirationDate);
-        return new ResponseEntity<>(products, HttpStatus.OK);
+    public ResponseEntity<List<Product>> getProductsExpiringBefore(@RequestParam("date") LocalDate date) {
+        try {
+            return ResponseEntity.ok(productService.getProductsExpiringBefore(date));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/filter/price")
-    public ResponseEntity<List<Product>> getProductsByPriceRange(@RequestParam("min") BigDecimal min, @RequestParam("max") BigDecimal max) {
-        List<Product> products = productService.getProductsByPriceRange(min, max);
-        return new ResponseEntity<>(products, HttpStatus.OK);
+    public ResponseEntity<List<Product>> getProductsByPriceRange(
+            @RequestParam("min") BigDecimal min,
+            @RequestParam("max") BigDecimal max
+    ) {
+        try {
+            return ResponseEntity.ok(productService.getProductsByPriceRange(min, max));
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
